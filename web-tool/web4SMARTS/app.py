93a199ddc721bbb6bf246a8f39882edd089212b2
zhizhaowa <zhizhaow@ucr.edu>
2026-01-27 22:25:01 +0000

    Use same settings for testing

--- web-tool/web4SMARTS/app.py
+++ web-tool/web4SMARTS/app.py
@@ -40,6 +40,6 @@ def rdk():
     )
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=5005)
+    app.run(host="0.0.0.0", port=5005)
     # Localhost
-    app.run(host="127.0.0.1", port=5005)
+    # app.run(host="127.0.0.1", port=5005)
--- web-tool/web4draw/app.py
+++ web-tool/web4draw/app.py
@@ -59,5 +59,5 @@ def svg():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000)
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(host="127.0.0.1", port=5000)
